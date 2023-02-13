5088e98cb9bc7a16e651e71f969026881aba2d1a
RAVISHANKAR J <84534945+yuvaravii@users.noreply.github.com>
2023-02-13 19:39:31 +0530

    A mind map of my perspective and research done.
    Use freemind(open source and free) software to view the mind map.

---
+++ Sentiment Analysis.mm
@@ -0,0 +1,10 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1676295318734" ID="ID_163781024" MODIFIED="1676295332127" TEXT="Sentiment Analysis">
+<node CREATED="1676295338034" ID="ID_1853367707" MODIFIED="1676295508988" POSITION="right" TEXT="what are the outputs of sentiment analysis ? and how this output helps business ?"/>
+<node CREATED="1676295355590" ID="ID_656070921" MODIFIED="1676295367447" POSITION="right" TEXT="What exactly do we get out of this work ?"/>
+<node CREATED="1676295368369" ID="ID_1656115427" MODIFIED="1676295379657" POSITION="right" TEXT="Who will benefit from this analysis ?"/>
+<node CREATED="1676295385957" ID="ID_41654320" MODIFIED="1676295405016" POSITION="right" TEXT="How it helps the audience ?"/>
+<node CREATED="1676295471721" ID="ID_657303879" MODIFIED="1676295490580" POSITION="right" TEXT="How to measure how this sentiment analysis is effective ?"/>
+</node>
+</map>
